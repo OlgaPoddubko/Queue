4413ce3b3692513236c4e9fde7b2c287e13a1f4b
Olga <olgapoddubko@gmail.com>
2016-09-14 10:55:34 +0300

    add code

--- src/queue.js
+++ src/queue.js
@@ -2,23 +2,36 @@ const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
 	constructor(maxSize) {
+		if (maxSize) {
+			this.maxSize = maxSize;
+		} else {this.maxSize = 30;
+		}
 
+		this.heap = new MaxHeap ();
 	}
 
 	push(data, priority) {
-
+		if (this.heap.size() < this.maxSize) {
+		this.heap.push(data,priority);
+		} else {
+			throw new Error ("This queue already has max size");
+		}
 	}
 
 	shift() {
-
+		if(this.heap.isEmpty()){
+			throw "empty";
+		} else{
+			return this.heap.pop();
+		}
 	}
 
 	size() {
-
+		return this.heap.size();
 	}
 
 	isEmpty() {
-		
+		return this.heap.isEmpty();
 	}
 }
 
